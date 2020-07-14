3407cfa0fdb8f325d63ea4c285ebc35b7dcc0181
Andrey <kouznts@ya.ru>
2020-07-14 15:48:52 +0400

    impl start input

--- contacts/src/contacts/Main.java
+++ contacts/src/contacts/Main.java
@@ -1,8 +1,24 @@
 package contacts;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Contact contact = new Contact();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the name of the person:\n> ");
+        contact.setName(scanner.nextLine());
+        System.out.print("Enter the surname of the person:\n> ");
+        contact.setSurname(scanner.nextLine());
+        System.out.print("Enter the number::\n> ");
+        contact.setPhoneNumber(scanner.nextLine());
+        System.out.println();
+
+        PhoneBook phoneBook = new PhoneBook(contact);
+        System.out.println("A record created!");
+        System.out.println("A Phone Book with a single record created!");
     }
 }
