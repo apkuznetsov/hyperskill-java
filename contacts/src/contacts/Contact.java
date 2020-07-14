532d10564680bfbf993749ecfa9f89f79db1d525
Andrey <kouznts@ya.ru>
2020-07-14 21:06:01 +0400

    impl check phone number

--- contacts/src/contacts/Contact.java
+++ contacts/src/contacts/Contact.java
@@ -1,5 +1,8 @@
 package contacts;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Contact {
 
     private String name;
@@ -36,6 +39,20 @@ public class Contact {
     }
 
     public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+        if (isPhoneNumber(phoneNumber)){
+            this.phoneNumber = phoneNumber;
+        } else {
+            System.out.println("Wrong number format!");
+            this.phoneNumber = "[no number]";
+        }
+    }
+
+    private boolean isPhoneNumber(String phoneNumber) {
+        Pattern pattern = Pattern.compile("\\+?" +
+                "((\\([0-9A-Za-z]+\\)|[0-9A-Za-z]+)"
+                +"|([0-9A-Za-z]+[ -]\\([0-9A-Za-z]{2,}\\))|[0-9A-Za-z]+[ -][0-9A-Za-z]{2,})"
+                +"([ -][0-9A-Za-z]{2,}[ -]?)*");
+        Matcher matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
     }
 }
