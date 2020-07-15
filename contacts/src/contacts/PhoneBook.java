075b15b491d93a148add90744dee6106aa48984e
Andrey <kouznts@ya.ru>
2020-07-15 16:52:53 +0400

    impl phone book methods

--- contacts/src/contacts/PhoneBook.java
+++ contacts/src/contacts/PhoneBook.java
@@ -1,13 +1,37 @@
 package contacts;
 
-import org.jetbrains.annotations.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhoneBook {
 
-    final private Contact firstContact;
+    final private List<Contact> contacts = new ArrayList<>();
 
-    public PhoneBook(@NotNull Contact contact) {
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
 
-        firstContact = contact;
+    public void removeContact(int id) {
+        contacts.remove(id - 1);
+    }
+
+    public void editContact(int id, Contact editedContact) {
+        contacts.set(id - 1, editedContact);
+    }
+
+    public Contact getContact(int id) {
+        return contacts.get(id - 1);
+    }
+
+    public int getPhoneBookSize() {
+        return contacts.size();
+    }
+
+    public void printAllContacts() {
+        int i = 1;
+        for (Contact c : contacts) {
+            System.out.println(i + ". " + c);
+            i++;
+        }
     }
 }
