2b9349eb5c3b8203ebcce5d168aea9f6e3330ec9
Andrey <kouznts@ya.ru>
2020-07-16 12:35:35 +0400

    add edited at date to contact

--- contacts/src/contacts/contacts/Contact.java
+++ contacts/src/contacts/contacts/Contact.java
@@ -1,16 +1,21 @@
 package contacts.contacts;
 
+import java.time.LocalDate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Contact {
 
+    private final LocalDate createdAt;
     private String name;
     private String phoneNumber = "[no number]";
+    private LocalDate lastEditedAt;
 
     public Contact(String name, String phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
+        createdAt = LocalDate.now();
+        lastEditedAt = createdAt;
     }
 
     public String getName() {
@@ -19,6 +24,7 @@ public class Contact {
 
     public void setName(String name) {
         this.name = name;
+        lastEditedAt = createdAt;
     }
 
     public String getPhoneNumber() {
@@ -26,8 +32,9 @@ public class Contact {
     }
 
     public void setPhoneNumber(String phoneNumber) {
-        if (isPhoneNumber(phoneNumber)){
+        if (isPhoneNumber(phoneNumber)) {
             this.phoneNumber = phoneNumber;
+            lastEditedAt = createdAt;
         } else {
             System.out.println("Wrong number format!");
             this.phoneNumber = "[no number]";
@@ -37,10 +44,17 @@ public class Contact {
     private boolean isPhoneNumber(String phoneNumber) {
         Pattern pattern = Pattern.compile("\\+?" +
                 "((\\([0-9A-Za-z]+\\)|[0-9A-Za-z]+)"
-                +"|([0-9A-Za-z]+[ -]\\([0-9A-Za-z]{2,}\\))|[0-9A-Za-z]+[ -][0-9A-Za-z]{2,})"
-                +"([ -][0-9A-Za-z]{2,}[ -]?)*");
+                + "|([0-9A-Za-z]+[ -]\\([0-9A-Za-z]{2,}\\))|[0-9A-Za-z]+[ -][0-9A-Za-z]{2,})"
+                + "([ -][0-9A-Za-z]{2,}[ -]?)*");
         Matcher matcher = pattern.matcher(phoneNumber);
         return matcher.matches();
     }
 
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDate getLastEditedAt() {
+        return lastEditedAt;
+    }
 }
