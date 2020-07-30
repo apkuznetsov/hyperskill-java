a8809558a2a53176364569459c2711e55cc58cea
Andrey <kouznts@ya.ru>
2020-07-30 11:04:12 +0400

    implementation

--- html-parser/src/htmlparser/Main.java
+++ html-parser/src/htmlparser/Main.java
@@ -1,39 +1,44 @@
 package htmlparser;
 
 import java.util.Scanner;
-import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
-    private static final String TEST_STR = "<html><a>hello</a><h1><h4>nestedHello</h4><h3>nestedWorld</h3><h6><br>top</br></h6></h1><br>world</br></html>";
-
-    private static final Pattern javaPattern = Pattern.compile("(.*?<.*?>)(.*)(</.*?>.*?)", Pattern.CASE_INSENSITIVE);
-    private static final Pattern htmlPattern = Pattern.compile("(.*?<html>)(.*)(</html>.*?)", Pattern.CASE_INSENSITIVE);
+    private static final Pattern tagPattern = Pattern.compile("<([a-zA-Z0-9]+)>(.+?)</\\1>");
 
     public static void main(String[] args) {
-        //Scanner scanner = new Scanner(System.in);
-        //process(TEST_STR);
+        final Scanner scanner = new Scanner(System.in);
+        final String input = scanner.nextLine();
 
-        Stack<String> outputStack = new Stack<>();
-
-        Matcher matcher = htmlPattern.matcher(TEST_STR);
-        if (matcher.find()) {
-            outputStack.add(matcher.group(2));
+        final Matcher matcher = tagPattern.matcher(input);
+        while (matcher.find()) {
+            recursivePrintln(matcher.group(2));
         }
 
-        System.out.println(outputStack.pop());
-
+        scanner.close();
     }
 
-    private static void process(final String input) {
-        Matcher matcher = htmlPattern.matcher(input);
-        if (matcher.find()) {
-            System.out.println(matcher.group(2));
-            process(matcher.group(2));
-        } else {
+    /* processInput()
+     * If it has no children Then
+     * * Print content
+     * else
+     * * For each child processInput()
+     * * * Print child content */
 
+    private static void recursivePrintln(String input) {
+        final Matcher hasChildrenMatcher = tagPattern.matcher(input);
+        if (!hasChildrenMatcher.find()) {
+            System.out.println(input);
+            return;
         }
+
+        Matcher eachChildMatcher = tagPattern.matcher(input);
+        while (eachChildMatcher.find()) {
+            recursivePrintln(eachChildMatcher.group(2));
+        }
+
+        System.out.println(input);
     }
 }
